a38de5bab0a799fc9b2076dc5d73051efc5e93c6
Alexander Bifulco <alexander.bifulco@gmail.com>
2019-06-29 09:48:08 +0200

    drawing a triangle

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,23 @@
 extern crate glfw;
 extern crate gl;
+use self::gl::types::*;
+
 extern crate cgmath;
 
 mod graphic;
 
 use graphic::canvas;
 use graphic::shader;
-use cgmath::Vector3;
+
+use std::mem;
+use std::os::raw::c_void;
+use std::ptr;
+use std::str;
 
 // settings
 const TITLE: &str = "OpenGL";
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
-
-// glm::vec3 vertices_rectangle[] = {
-//     {-0.5f, 0.5f, 0.0f},
-//     {-0.5f, -0.5f, 0.0f},
-//     {0.5f, -0.5f, 0.0f},
-//     {0.5f, 0.5f, 0.0f}};
-// glm::uvec3 index_rectangle[] = {{0, 1, 2}, {2, 3, 0}};
-
-// glm::vec3 vertices_cube[] = {
-//     // front up left 0
-//     {-0.5f, 0.5f, 0.5f},
-//     // front down left 1
-//     {-0.5f, -0.5f, 0.5f},
-//     // front down right 2
-//     {0.5f, -0.5f, 0.5f},
-//     // front up right 3
-//     {0.5f, 0.5f, 0.5f},
-
-//     // back up left 4
-//     {-0.5f, 0.5f, -0.5f},
-//     // back down left 5
-//     {-0.5f, -0.5f, -0.5f},
-//     // back down right 6
-//     {0.5f, -0.5f, -0.5f},
-//     // back up right 7
-//     {0.5f, 0.5f, -0.5f},
-// };
-// glm::uvec3 index_cube[] = {
-//     // front face
-//     {0, 1, 2},
-//     {2, 3, 0},
-//     // back face
-//     {7, 6, 5},
-//     {5, 4, 6},
-//     // left face
-//     {4, 5, 1},
-//     {1, 0, 4},
-//     // right face
-//     {3, 2, 6},
-//     {6, 7, 3},
-// };
-
-// // float pyramide
-// glm::vec3 vertices_pyramide[] = {
-//     // corner
-//     {0.0f, 0.5f, 0.0f},
-//     // front left
-//     {0.5f, -0.5f, 0.0f},
-//     // front right
-//     {-0.5f, -0.5f, 0.0f},
-//     // back
-//     {0.0f, -0.5f, 0.5f},
-
-// };
-
-// glm::uvec3 index_pyramide[] = {
-//     {0, 1, 2},
-//     {0, 2, 3},
-//     {0, 3, 1},
-// };
 
 pub fn main() {
     // glfw: initialize and configure
@@ -82,46 +28,64 @@ pub fn main() {
     // ---------------------------------------
     canvas.load_graphic_library_functions();
 
-    let shader = shader::Shader::new("src/assets/vertex.shader.glsl", "src/assets/fragment.shader.glsl");
+    let shader = shader::Shader::new(
+        "src/assets/vertex.shader.glsl",
+        "src/assets/fragment.shader.glsl",
+    );
 
-    let vertices_triangle: [Vector3<f32>; 3] = [
-        Vector3::new(0.0, 1.0, 0.0),
-        Vector3::new(1.0, -1.0, 0.0),
-        Vector3::new(-1.0, -1.0, 0.0),
+    let vertices_triangle: [f32; 9] = [
+        0.0, 1.0, 0.0,
+        1.0, -1.0, 0.0,
+        -1.0, -1.0, 0.0
     ];
-
-    let index_triangle: [Vector3<i32>; 1] = [
-        Vector3::new(0, 1, 2),
-    ];
+    let index_triangle = [0, 1, 2];
 
     let mut vao = 0;
     let mut vbo = 0;
     let mut ibo = 0;
 
-    unsafe {
-        gl::GenVertexArrays(1, &mut vao);
-        gl::BindVertexArray(vao);
+   unsafe {
+        // set up vertex data (and buffer(s)) and configure vertex attributes
+        // ------------------------------------------------------------------
+        // HINT: type annotation is crucial since default for float literals is f64
 
+        gl::GenVertexArrays(1, &mut vao);
         gl::GenBuffers(1, &mut vbo);
         gl::GenBuffers(1, &mut ibo);
+        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+        gl::BindVertexArray(vao);
 
         gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        // glBufferData(gl::ARRAY_BUFFER, verticesSize, vertices, gl::STATIC_DRAW);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices_triangle.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            &vertices_triangle as *const f32 as *const c_void,
+            gl::STATIC_DRAW,
+        );
 
-        // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-        gl::EnableVertexAttribArray(0);
         gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ibo);
-        // glBufferData(gl::ELEMENT_ARRAY_BUFFER, indicesSize, indicies, gl::STATIC_DRAW);
-    }
+        gl::BufferData(
+            gl::ELEMENT_ARRAY_BUFFER,
+            (index_triangle.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            &index_triangle as *const i32 as *const c_void,
+            gl::STATIC_DRAW,
+        );
+
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            3 * mem::size_of::<GLfloat>() as GLsizei,
+            ptr::null(),
+        );
+
+        gl::EnableVertexAttribArray(0);
+    };
 
     // render loop
     // -----------
     while !canvas.should_close {
-
-        let delta_time: f64 = unsafe {
-            glfw::ffi::glfwGetTime() as f64
-        };
-
         // events
         // -----
         canvas.process_events();
@@ -131,6 +95,14 @@ pub fn main() {
         unsafe {
             gl::ClearColor(0.2, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            // draw our first triangle
+            shader.use_program();
+
+            gl::BindVertexArray(vao); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+                                      // gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
+            // glBindVertexArray(0); // no need to unbind it every time
         }
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
